8c6450017c41d75acd77f116cbf6894544f9c52f
msawaiz1 <muhammadsawaiz8@gmail.com>
2023-11-04 14:30:34 +0500

    Dashboard

--- sms/smsapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Students
-# Register your models here.
-
-modelslist = [Students]
-
-admin.site.register(modelslist)
--- sms/smsapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .serializer import StudentSerializer
-from .models import Students
-from .views import StudentView
-
-urlpatterns = [
-    path("students/", StudentView.as_view()),
-]
-
